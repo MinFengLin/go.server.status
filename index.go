1b8a94aae537e2a34fff1af643cce900b81d9ffc
Minfeng <atminfeng@gmail.com>
2022-08-01 17:35:54 +0800

    PHPTOGO-1:
    - IotService: Use json to parser

--- index.go
+++ index.go
@@ -1,65 +1,63 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-// // Book ...
-// type Book struct {
-// 	Title  string
-// 	Author string
-// }
-
-// IoTservices
-type IoTservices struct {
-	// 開頭一定要大寫
-	Ip      string
-	Service string
-	Power   string
+// https://stackoverflow.com/questions/64693710/parse-json-file-in-golang
+type IoTservices_Data struct {
+	IoTservices []IoTservices `json:"j_IoTservices"`
 }
+
+type IoTservices struct {
+	Name    string `json:"Name"`
+	Ip      string `json:"Ip"`
+	Service string `json:"Service"`
+	Power   string `json:"Power"`
+
+	// Request Request `json:"request"`
+}
+
+// type Request struct {
+// 	Method string `json:"method"`
+// }
 
 func main() {
 	server := gin.Default()
 	server.LoadHTMLFiles("index.html")
 
-	services := make([]IoTservices, 0)
+	filename := "iotservice.json"
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("failed to open json file: %s, error: %v", filename, err)
+		return
+	}
+	defer jsonFile.Close()
 
-	services = append(services, IoTservices{
-		Ip:      "192.168.1.1",
-		Service: "Author 1",
-		Power:   "OFF",
-	})
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Printf("failed to read json file, error: %v", err)
+		return
+	}
 
-	services = append(services, IoTservices{
-		Ip:      "192.168.1.2",
-		Service: "Author 1",
-		Power:   "ON",
-	})
+	data := IoTservices_Data{}
+	if err := json.Unmarshal(jsonData, &data); err != nil {
+		fmt.Printf("failed to unmarshal json file, error: %v", err)
+		return
+	}
 
 	server.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"IoTservices": services,
+			"IoTservices": data.IoTservices,
 		})
 	})
-	// books := make([]Book, 0)
-	// books = append(books, Book{
-	// 	Title:  "Title 1",
-	// 	Author: "Author 1",
-	// })
-	// books = append(books, Book{
-	// 	Title:  "Title 2",
-	// 	Author: "Author 2",
-	// })
-
-	// server.GET("/", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "index.html", gin.H{
-	// 		"books": books,
-	// 	})
-	// })
 
 	log.Fatal(server.Run(":8222"))
-
 }
