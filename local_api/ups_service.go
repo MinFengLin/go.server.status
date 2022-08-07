b0cfb1d421ce8457655d57e0268b97777885e8bc
Minfeng <minfenglin0406@gmail.com>
2022-08-07 09:28:11 +0000

    PHPTOGO-1: Add ups code
    - not check if it work just add code

---
+++ local_api/ups_service.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// https://stackoverflow.com/questions/64693710/parse-json-file-in-golang
+// http://puremonkey2010.blogspot.com/2020/11/parsing-json-files-with-golang.html read
+
+// Start -  json struct
+type UpsInfo struct {
+	Battery     Battery_s `json:"battery"`
+	Input       Input_s   `json:"input"`
+	Upsfullinfo Ups_s     `json:"ups"`
+}
+// Battery
+type Battery_s struct {
+	Charge  string `json:"charge"`
+	Runtime string `json:"runtime"`
+	Type    string `json:"type"`
+	Voltage string `json:"voltage"`
+}
+// Input
+type Input_s struct {
+	Voltage string `json:"voltage"`
+}
+// UPS full info
+type Ups_s struct {
+	Beeper UPS_Beeper_s `json:"beeper"`
+	Load   string       `json:"load"`
+	Mfr    string       `json:"mfr"`
+	Model  string       `json:"model"`
+	Serial string       `json:"serial"`
+	Status string       `json:"status"`
+	Test   string       `json:"test"`
+}
+
+type UPS_Beeper_s struct {
+	Voltage string `json:"status"`
+}
+// End -  json struct
+
+// TODO: (down) not check if it correct
+
+func Parser_upsinfo() UpsInfo {
+
+	filename := "./local_api/go_ups_data.json"
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("failed to open json file: %s, error: %v", filename, err)
+	}
+	defer jsonFile.Close()
+
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Printf("failed to read json file, error: %v", err)
+	}
+
+	data := UpsInfo{}
+	if err := json.Unmarshal(jsonData, &data); err != nil {
+		fmt.Printf("failed to unmarshal json file, error: %v", err)
+	}
+
+	// fmt.Printf("%+v\n", data)
+
+	return data
+}
